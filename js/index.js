9f2fb01567a78e588488b7678fe2ddada9cee61b
MAS3GO <231335@virtualwindow.co.za>
2023-05-12 16:58:16 +0200

    task one
    task one , data types

---
+++ js/index.js
@@ -0,0 +1,35 @@
+//exercise one
+
+var firstName = "Masego"
+let surname = "Matlotleng"
+let age = 31
+let subject = "Interactive Development"
+
+let message = "Hello, my name is " + firstName + " " + surname + " I am " + age + " years old and i am studying " + subject + "."
+console.log(message);
+
+//exercise two
+
+let numOne = 10
+let numTwo = 15
+let numThree = 1000
+let numFour = 8
+let answer = 0
+
+let total = numOne + numTwo / numThree + numFour
+let success = answer
+
+console.log(answer)
+
+
+
+//exercise three
+
+let timmy = 1.50
+let susan = 3.75
+let taxMan = timmy + susan 
+let victory = taxMan
+
+console.log(victory)
+
+//exercise four
